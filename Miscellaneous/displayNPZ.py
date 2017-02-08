0e92e0453fcf028898c28e16df887951f827fa21
romanjimenez-g <geoffrey.romanjimenez@univ-nantes.fr>
2017-02-08 09:52:58 +0100

    modified:   displayNPZ.py

--- Miscellaneous/displayNPZ.py
+++ Miscellaneous/displayNPZ.py
@@ -33,29 +33,28 @@ def read_npz(filename):
 def main():
 	parser = argparse.ArgumentParser(description="open [file.npz] and convert it into csv")
 	parser.add_argument("FILENPZ", help="fileName of the input array")
-	parser.add_argument("-s", dest="seq", help="list of number", nargs="+", type=int)
 
 	args = parser.parse_args()
-	lala = args.seq
-	print(lala)
 	fnp = read_npz(args.FILENPZ)
+	print(fnp.keys())
 	nameHeader = ["OptNbSample_ns", "OptMseTrain_ns", "OptMseValid_ns","TensorMseTrain_ns","TensorMseValid_ns","OptNbSample_s","OptAccTrain_s","OptAceTrain_s","OptMseValid_s","OptAccValid_s","OptAceValid_s","TensorMseValid_s","TensorAceTrain_s","TensorAceValid_s","TensorAccValid_s","ArrayAccTest","ArrayAceTest","ArrayMseTest"]
-	AccTest = getattr( fnp.f, 'ArrayAccTest').T
-	AceTest = getattr( fnp.f, 'ArrayAceTest').T
-	MseTest = getattr( fnp.f, 'ArrayMseTest').T
+	AccTestclass = getattr( fnp.f, 'ArrayAccTest_class').T
+	AccTestcnn = getattr( fnp.f, 'ArrayAccTest_cnn').T
+	AccTestmlp = getattr( fnp.f, 'ArrayAccTest_mlp').T
+# 	MseTest = getattr( fnp.f, 'ArrayMseTest_class').T
 
-	fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 10), sharey=False, sharex=True)
+	fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 10), sharey=True, sharex=True)
 	axes[0].set_ylim(0.8, 1.05)
 	#axes[1].set_ylim(0.05, 1.05)
 	#axes[2].set_ylim(0.05, 0.5)
-
-	dfAccTest = pd.DataFrame( AccTest )
-	dfAccTest.plot.box(ax=axes[0], title='Accuracy of classification TEST depending on Supervisation Rate (Supervised/Unsupervised)')
-	print(dfAccTest)
-	dfAceTest = pd.DataFrame( AceTest )
-	dfAceTest.plot.box(ax=axes[1], title='Average Cross Entropy on classification TEST depending on Supervisation Rate (Supervised/Unsupervised)')
-	dfMseTest = pd.DataFrame( MseTest )
-	dfMseTest.plot.box(ax=axes[2], title='Mean Square Error on reconstruction TEST depending on Supervisation Rate (Supervised/Unsupervised)')
+	labcols=[100,90,80,70,60,50,40,30,20,10,9,8,7,6,5,4,3,2,1,0]
+	dfAccTestclass = pd.DataFrame( AccTestclass, columns=labcols )
+	dfAccTestclass.plot.box(ax=axes[2], title='Classification accuracy of Local Feature AutoEncoder (LFAE) depending on Supervisation Rate (Supersised/Unsupervised)')
+	print(dfAccTestclass)
+	dfAccTestmlp = pd.DataFrame( AccTestmlp , columns=labcols )
+	dfAccTestmlp.plot.box(ax=axes[0], title='Classification accuracy of MLP depending on number of training data (given by the Supervisation Rate)')
+	dfAccTestcnn = pd.DataFrame( AccTestcnn , columns=labcols )
+	dfAccTestcnn.plot.box(ax=axes[1], title='Classification accuracy of CNN depending on the number of training data used (given by the Supervisation Rate)')
 	plt.show()
 
 
